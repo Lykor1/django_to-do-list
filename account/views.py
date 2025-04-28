87f91f3632638a9386782b4d26ee67bddb6092d3
Evgen <evgeshkarach@gmail.com>
2025-04-28 10:36:18 +0400

    Исправлены шаблоны

--- account/views.py
+++ account/views.py
@@ -10,13 +10,13 @@ from .forms import UserRegistrationForm
 class TaskLoginView(LoginView):
     template_name = 'account/login.html'
 
-    def form_invalid(self, form):
-        messages.error(self.request, 'Неверное имя пользователя или пароль. Пожалуйста, попробуйте снова.')
-        return super().form_invalid(form)
-
     def form_valid(self, form):
         messages.success(self.request, f"Добро пожаловать, {form.get_user().username}!")
         return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Проверьте адрес электронной почты и пароль.')
+        return super().form_invalid(form)
 
 
 class TaskLogoutView(LogoutView):
@@ -35,19 +35,11 @@ class TaskPasswordChangeView(PasswordChangeView):
         messages.success(self.request, 'Пароль успешно изменён!')
         return super().form_valid(form)
 
-    def form_invalid(self, form):
-        messages.error(self.request, 'Неудача! Проверьте данные!')
-        return super().form_invalid(form)
-
 
 class TaskPasswordResetView(PasswordResetView):
     email_template_name = 'account/password_reset_email.html'
     template_name = 'account/password_reset_form.html'
     success_url = reverse_lazy('account:password_reset_done')
-
-    def form_invalid(self, form):
-        messages.error(self.request, 'Неудача! Проверьте адрес электронной почты!')
-        return super().form_invalid(form)
 
 
 class TaskPasswordResetDoneView(PasswordResetDoneView):
@@ -61,10 +53,6 @@ class TaskPasswordResetConfirmView(PasswordResetConfirmView):
     def form_valid(self, form):
         messages.success(self.request, 'Пароль успешно изменён! Теперь вы можете войти.')
         return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, 'Неудача! Проверьте данные!')
-        return super().form_invalid(form)
 
 
 def register(request):
